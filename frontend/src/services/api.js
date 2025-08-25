11220a4d0a24bf8670f315b6efd6abf6130a94a2
Bawiloussim <bawiloussimngbabou1@gmail.com>
2025-08-25 13:01:22 +0000

    dotenv config

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+// const API_URL = 'http://localhost:5000/api'; // local API_URL
+
+const API_URL = import.meta.env.VITE_API_BASE_URL; //Production API_URL
 
 const api = axios.create({
     baseURL: API_URL,
